5b3ca4a7ac9544d9ff94a809d347f8142b3568fb
giraggio <70457115+giraggio@users.noreply.github.com>
2025-03-27 10:20:11 -0300

    Update buscador_palabras.py
    borrar hamburguer y footer

--- buscador_palabras.py
+++ buscador_palabras.py
@@ -48,3 +48,11 @@ if archivo is not None:
 
         else:
             st.warning("No se encontraron coincidencias.")
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
